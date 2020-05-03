fe04c442e39b153237dfbbf1080cd71443b8e048
attacker145 <chakr1@yahoo.com>
2020-05-03 09:41:07 -0700

    Update main_Rev01.c

--- examples/ble_peripheral/ble_app_uart/main_Rev01.c
+++ examples/ble_peripheral/ble_app_uart/main_Rev01.c
@@ -1,28 +1,3 @@
-/* Copyright (c) 2014 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- */
-
-/** @file
- *
- * @defgroup ble_sdk_uart_over_ble_main main.c
- * @{
- * @ingroup  ble_sdk_app_nus_eval
- * @brief    UART over BLE application main file.
- *
- * This file contains the source code for a sample application that uses the Nordic UART service.
- * This application uses the @ref srvlib_conn_params module.
- * Additional files:
-   1. nrf_drv_spi.c 
- */
-
 #include <stdint.h>
 #include <string.h>
 #include "nordic_common.h"
